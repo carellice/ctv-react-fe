684de223366d66e50617693dcdb0767b5e7b2da3
carellice <100231222+carellice@users.noreply.github.com>
2024-03-01 09:58:12 +0100

    2.4
    - added data backup json esportazione
    - added barra di ricerca in svago e prima necessità

--- src/pages/PrimaNecessita.js
+++ src/pages/PrimaNecessita.js
@@ -5,7 +5,7 @@ import ListPersonal from '../components/ListPersonal';
 import * as DataBaseUtils from "./../utils/DataBaseUtils";
 import * as SnackBarUtils from "./../utils/SnackBarUtils";
 import * as ImportoUtils from "./../utils/ImportoUtils";
-import { Grow, Typography } from '@mui/material';
+import {Grow, InputAdornment, OutlinedInput, Typography} from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Box from '@mui/material/Box';
 import Radio from '@mui/material/Radio';
@@ -16,10 +16,68 @@ import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import { isApp } from '../Config';
 import * as OrderByUtils from "../utils/OrderByUtils";
+import IconButton from "@mui/material/IconButton";
+import BackspaceIcon from "@mui/icons-material/Backspace";
+import SearchIcon from "@mui/icons-material/Search";
 
 function PrimaNecessita({ setSelectedPage, snackBarFunc, primaNecessita, update }) {
   //NASCONDE AUTOMATICAMENTE IL FAB
   const [showFab, setShowFab] = useState(true);
+
+  const [openPopUpInsert, setOpenPopUpInsert] = useState(false);
+  const [openPopUpEdit, setOpenPopUpEdit] = useState(false);
+  //ORDER BY
+  const [orderBy, setOrderBy] = useState(OrderByUtils.orderByNome);
+
+  // LISTA DI PRIMA NECESSITA'
+  const [primaNecessitaArray, setPrimaNecessitaArray] = useState(primaNecessita);
+  //BARRA DI RICERCA
+  const [searchValue, setSearchValue] = useState("");
+
+  //FUNZIONE PER LA RICERCA DI SVAGO
+  const ricerca = (searchValue) => {
+    const newPrimaNecessitaArray1 = primaNecessita.filter(s => s.nome.toLowerCase().includes(searchValue.toLowerCase()));
+    const newPrimaNecessitaArray2 = primaNecessita.filter(s => s.note.toLowerCase().includes(searchValue.toLowerCase()));
+
+    // Unisci i due array
+    let mergedArray = [...newPrimaNecessitaArray1, ...newPrimaNecessitaArray2];
+
+    //controllo se ordinato per nome o per costo
+    if (OrderByUtils.orderByNome === orderBy) {
+      mergedArray = mergedArray.sort((a, b) => {
+        const nomeA = a.nome.toUpperCase(); // Converti in maiuscolo per l'ordinamento senza distinzione tra maiuscole e minuscole
+        const nomeB = b.nome.toUpperCase();
+
+        if (nomeA < nomeB) {
+          return -1;
+        }
+        if (nomeA > nomeB) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (OrderByUtils.orderByCosto === orderBy) {
+      mergedArray = mergedArray.sort((a, b) => {
+        const nomeA = parseFloat(a.costo);
+        const nomeB = parseFloat(b.costo);
+
+        if (nomeA < nomeB) {
+          return -1;
+        }
+        if (nomeA > nomeB) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+
+    // Utilizza un Set per ottenere valori univoci per id
+    const uniqueArray = Array.from(new Set(mergedArray.map(obj => obj.id))).map(id => {
+      return mergedArray.find(obj => obj.id === id);
+    });
+
+    setPrimaNecessitaArray(uniqueArray);
+  }
 
   useEffect(() => {
     DataBaseUtils.orderPrimaNecessitaByNome().then(() => update());
@@ -59,11 +117,6 @@ function PrimaNecessita({ setSelectedPage, snackBarFunc, primaNecessita, update 
     });
   };
 
-  const [openPopUpInsert, setOpenPopUpInsert] = useState(false);
-  const [openPopUpEdit, setOpenPopUpEdit] = useState(false);
-  //ORDER BY
-  const [orderBy, setOrderBy] = useState(OrderByUtils.orderByNome);
-
   return (
     <>
       {primaNecessita.length === 0 ? (
@@ -83,6 +136,19 @@ function PrimaNecessita({ setSelectedPage, snackBarFunc, primaNecessita, update 
                     control={<Radio onClick={() => {
                       DataBaseUtils.orderPrimaNecessitaByNome().then(() => update());
                       setOrderBy(OrderByUtils.orderByNome);
+                      const newArray = primaNecessitaArray.sort((a, b) => {
+                        const nomeA = a.nome.toUpperCase(); // Converti in maiuscolo per l'ordinamento senza distinzione tra maiuscole e minuscole
+                        const nomeB = b.nome.toUpperCase();
+
+                        if (nomeA < nomeB) {
+                          return -1;
+                        }
+                        if (nomeA > nomeB) {
+                          return 1;
+                        }
+                        return 0;
+                      });
+                      setPrimaNecessitaArray(newArray);
                     }} />}
                     label="Nome"
                     labelPlacement="end" // Imposta la posizione della label a destra del radio button
@@ -93,6 +159,19 @@ function PrimaNecessita({ setSelectedPage, snackBarFunc, primaNecessita, update 
                     control={<Radio onClick={() => {
                       DataBaseUtils.orderPrimaNecessitaByCosto().then(() => update());
                       setOrderBy(OrderByUtils.orderByCosto);
+                      const newArray = primaNecessitaArray.sort((a, b) => {
+                        const nomeA = parseFloat(a.costo);
+                        const nomeB = parseFloat(b.costo);
+
+                        if (nomeA < nomeB) {
+                          return -1;
+                        }
+                        if (nomeA > nomeB) {
+                          return 1;
+                        }
+                        return 0;
+                      });
+                      setPrimaNecessitaArray(newArray);
                     }} />}
                     label="Costo"
                     labelPlacement="end" // Imposta la posizione della label a destra del radio button
@@ -114,9 +193,41 @@ function PrimaNecessita({ setSelectedPage, snackBarFunc, primaNecessita, update 
       ) : (
         <>
           <Typography style={{ textAlign: 'center', marginTop: 20, fontWeight: 'bold' }} variant='h5'>PRIMA NECESSITA' ({ImportoUtils.getImportoFormatted(ImportoUtils.sommaPrimaNecessita())} €)</Typography>
+
+          {/*BARRA DI RICERCA*/}
+          <Box display="flex" justifyContent="center" style={{marginTop:20, marginBottom:20}}>
+            <OutlinedInput
+                onChange={(event) => {
+                  setSearchValue(event.target.value);
+                  ricerca(event.target.value);
+                }}
+                value={searchValue}
+                id="outlined-adornment-password"
+                type={'text'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => {
+                          setSearchValue("");
+                          ricerca("");
+                        }}
+                        onMouseDown={() => {}}
+                        edge="end"
+                    >
+                      {searchValue !== "" ? <BackspaceIcon /> : <SearchIcon />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label=""
+            />
+          </Box>
+
+          {/*IN CASO NON CI FOSSERO RISULTATI DALLA RICERCA*/}
+          {primaNecessitaArray.length === 0 ? <Typography style={{ textAlign: 'center', marginTop: 20, marginBottom: 40, marginLeft: 20, marginRight: 20 }} variant='h6'>Nessun risultato!</Typography> : <></>}
         </>
       )}
-      <ListPersonal array={primaNecessita} editElement={editElement} openPopUpInsert={openPopUpInsert} setOpenPopUpInsert={setOpenPopUpInsert} />
+      <ListPersonal array={primaNecessitaArray} editElement={editElement} openPopUpInsert={openPopUpInsert} setOpenPopUpInsert={setOpenPopUpInsert} />
       <MyPopUpInsert saveFunc={saveElement} snackBarFunc={snackBarFunc} open={openPopUpInsert} setOpen={setOpenPopUpInsert} title={'INSERISCI PRIMA NECESSITA\''} />
       {localStorage.getItem("elToEdit") !== null ? (
         <MyPopUpEdit orderBy={orderBy} update={update} snackBarFunc={snackBarFunc} open={openPopUpEdit} setOpen={setOpenPopUpEdit} title={'MODIFICA PRIMA NECESSITA\''} />
